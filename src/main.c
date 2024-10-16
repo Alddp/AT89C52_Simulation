490a05f10e8396fe28c37db8f9504ff85c6397b6
Alddp <142954624+Alddp@users.noreply.github.com>
2024-10-16 16:52:58 +0800

    中断测试

--- src/main.c
+++ src/main.c
@@ -1,19 +1,22 @@
 #include <STC89C5xRC.H>
-#include "Int_DigitalTube.h"
-#include "Int_Key_Matrix.h"
+#include "Int_LED.h"
+#include "Com_Util.h"
 
+// 中断服务程序
+void test() interrupt 0
+{
+    D1 = ~D1; // 切换 LED 状态
+}
 void main()
 {
-    u8 result;
-    u8 *nums = 0;
+    EA  = 1; // 开启全局中断
+    EX0 = 1; // 使能外部中断0
+    IT0 = 1; // 设置为电平触发模式（IT0 = 0）
+
     while (1) {
-
-        result = Int_Key_Matrix_CheckS(); // 返回按下的按钮的编号
-
-        if (result) {
-            nums = Int_GetDisplayMemery(result); // 传入编号让数码管显示
-        }
-
-        Int_DigitalRefresh(nums); // 一直刷新数码管
+        IE0 = 1;
+        D2  = ~D2;
+        Com_Delay1s();
+        // Com_Delayms(200);
     }
 }
